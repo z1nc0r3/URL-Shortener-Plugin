52d9e91b3b5e89eeb81ba0a658d38dc2f7794dce
Lasith Manujitha <Manujitha98@gmail.com>
2024-02-04 07:59:27 +0530

    Core functions

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-# -*- coding: utf-8 -*-
+# Author: Lasith Manujitha
+# Github: @z1nc0r3
+# Description: A simple plugin to shorten URLs using cleanuri.com
+# Date: 2024-02-04
 
 import sys,os
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
@@ -8,22 +11,40 @@ sys.path.append(os.path.join(parent_folder_path, 'plugin'))
 
 from flowlauncher import FlowLauncher
 import webbrowser
-
+import requests
+import pyperclip
 
 class HelloWorld(FlowLauncher):
 
     def query(self, query):
-        return [
-            {
-                "Title": "Hello World, this is where title goes. {}".format(('Your query is: ' + query , query)[query == '']),
-                "SubTitle": "This is where your subtitle goes, press enter to open Flow's url",
-                "IcoPath": "Images/app.png",
-                "JsonRPCAction": {
-                    "method": "open_url",
-                    "parameters": ["https://github.com/Flow-Launcher/Flow.Launcher"]
-                }
-            }
-        ]
+        output=[]
+        if len(query.strip()) == 0:
+                output.append({
+                    "Title": "Enter a URL to shorten",
+                    "IcoPath": "Images/app.png"})
+        
+        else:
+            url = "https://cleanuri.com/api/v1/shorten"
+            payload = {'url': {query}}
+
+            response = requests.request("POST", url, data=payload)
+
+            print(response.text)
+            tiny = response.json()['result_url']
+                       
+            output.append({
+                    "Title": "Click to copy",
+                    "IcoPath": "Images/shorten.png",
+                    "JsonRPCAction": {"method": "copy", "parameters":[{tiny}]  }
+                    })
+            
+            output.append({
+                    "Title": "Click to open in browser",
+                    "IcoPath": "Images/shorten.png",
+                    "JsonRPCAction": {"method": "open_url", "parameters":[{tiny}]  }
+                    })
+            
+        return output
 
     def context_menu(self, data):
         return [
